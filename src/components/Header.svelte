56b53f764c0b4ff5f6f3b725b4b58342b1dc7f0b
Luca <l.d.mattiazzi@gmail.com>
2019-11-07 11:12:27 +0100

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,15 @@
 <script>
   import { Link } from 'svero'
+  const { random, floor } = Math
+  const titles = [
+    'Dada-driven developer',
+    'It\'s buggier on the inside',
+    'Stuff I\'m learning to play with',
+  ]
+  export let title = titles[floor(random() * titles.length)]
 </script>
 
 <div class="w-100 relative flex flex-column items-center justify-center pt3">
   <Link href="/" class="pa2 f1 no-underline black">Grokked it!</Link>
-  <div class="pa2 f4">It's buggier on the inside</div>
+  <div class="pa2 f4">{title}</div>
 </div>
