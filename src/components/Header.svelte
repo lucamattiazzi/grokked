f53faad1fff2c025447435fc2708bedde416e2be
Luca <l.d.mattiazzi@gmail.com>
2019-11-07 11:09:29 +0100

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,5 +4,5 @@
 
 <div class="w-100 relative flex flex-column items-center justify-center pt3">
   <Link href="/" class="pa2 f1 no-underline black">Grokked it!</Link>
-  <div class="pa2 f4">Stuff I'm learning to play with</div>
+  <div class="pa2 f4">It's buggier on the inside</div>
 </div>
